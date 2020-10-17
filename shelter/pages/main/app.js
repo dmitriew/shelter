fa38a76761f82939c1861ed47a56cb55e9d0fd28
dmitriew <hjq3301@mail.ru>
2020-10-17 18:13:59 +0700

    feat: add burger and modal

--- shelter/pages/main/app.js
+++ shelter/pages/main/app.js
@@ -1,53 +1,75 @@
-// const leftBtn = document.querySelector('.slider-button');
-// const rightBtn = document.querySelector('.slider-button_reverted');
-// const carousel = document.querySelector('.pets__wrapper');
-// const burger = document.querySelector('.burger');
+const leftBtn = document.querySelector('.slider-button'),
+      rightBtn = document.querySelector('.slider-button_reverted'),
+      carousel = document.querySelector('.pets__cards'),
+      slideposition = document.querySelector('.card'),
+      burger = document.querySelector('.burger'),
+      burgerList = document.querySelector('.burger__menu'),
+      burgerUl = document.querySelector('.burger__nav'),
+      headerWrapper = document.querySelector('.header__wrapper');
+      
 
-// function burgerMenu() {
-// burger.addEventListener('click', function() {
-//   console.log('burger click')
-//   if (burger.getAttribute('hidden') == 'true') {
-//     burger.removeAttribute('hidden')
-//   } else {
-//     burger.setAttribute ("hidden", "true")
-//   }
-// }); 
-// };
+function burgerMenu() {
+  burger.addEventListener('click', () => {
+    if (burgerList.hasAttribute('hidden')) {
+      burgerList.removeAttribute('hidden');
+      burger.style.transform = 'rotate(90deg)';
+      burgerUl.style.animationDirection = 'normal';
+      burgerUl.style.animation = 'burger-show 1s';
+      document.body.style.overflow = 'hidden';
+      
+
+    } else {
+      closeModal();
+      burgerUl.style.animationDirection = 'reverse';
+      setTimeout('burgerList.setAttribute("hidden", "true")', 1000);
+      burgerUl.style.animation = 'burger-hide 1s';
+      burger.style.transform = '';
+      document.body.style.overflow = 'visible';
+    }
+}); 
+};
+burgerList.addEventListener('click', (event) => {
+  if (event.target === burgerList) {
+    calcScroll();
+    burgerUl.style.animationDirection = 'reverse';
+    setTimeout('burgerList.setAttribute("hidden", "true")', 1000);
+    burgerUl.style.animation = 'burger-hide 1s';
+    burger.style.transform = '';
+    document.body.style.overflow = 'visible';
+  }
+    });
+
+function calcScroll() {
+  let div = document.createElement('div');
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.overflowY = 'scroll';
+  div.style.visibility = 'hidden';
+  document.body.appendChild(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+  return scrollWidth;
+};
 
 // function initCarousel() {
-//   let slidePositon = carousel.offsetWidth;
+//   let slidePositon = slideposition.offsetWidth;
 //   let position = 0;
 
-//   rightBtn.addEventListener( "click" , function() {
+//   rightBtn.addEventListener( "click" , () => {
+//     console.log('плик по правой')
 //       position -= slidePositon;
 //       movePositon();
-//       hideBtn();
 //   });
 
-//   leftBtn.addEventListener( "click" , function() {
+//   leftBtn.addEventListener( "click" , () => {
+//     console.log('плик по левой')
 //       position += slidePositon;
 //       movePositon();
-//       hideBtn();
 //   });
 
 //   const movePositon = () => {
 //     carousel.style.transform = `translateX(${position}px)`
 //   }
-
-//   const hideBtn = () => {
-//     if (position === 0) {
-//       leftBtn.style.display = 'none'
-//     } else {
-//       leftBtn.style.display = 'flex'
-//     };
-    
-//     if (position === -2964) {
-//       rightBtn.style.display = 'none'
-//     } else {
-//       rightBtn.style.display = 'flex'
-//     }
-//   }
-//   hideBtn();
 // }
 // initCarousel();
-// burgerMenu();
+burgerMenu();
